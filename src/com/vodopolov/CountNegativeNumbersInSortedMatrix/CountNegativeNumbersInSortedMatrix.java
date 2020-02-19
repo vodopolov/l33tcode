091fb5a647a3bdbbe08d47e103f0f643d9326957
Anton Vodopolov <vodopolov@devoler.com>
2020-02-19 23:00:51 +0200

    1351. Count Negative Numbers in a Sorted Matrix

---
+++ src/com/vodopolov/CountNegativeNumbersInSortedMatrix/CountNegativeNumbersInSortedMatrix.java
@@ -0,0 +1,23 @@
+package com.vodopolov.CountNegativeNumbersInSortedMatrix;
+
+/**
+ * Created by Anton on 19.02.2020.
+ */
+public class CountNegativeNumbersInSortedMatrix {
+    public static void main(String[] args) {
+        CountNegativeNumbersInSortedMatrix inst = new CountNegativeNumbersInSortedMatrix();
+        int[][] arr = { { 3, 2 }, { -1, -2 } };
+        System.out.println(inst.countNegatives(arr));
+    }
+
+    public int countNegatives(int[][] grid) {
+        int counter = 0;
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[i].length - 1; j >= 0; j--) {
+                if (grid[i][j] < 0) counter++;
+                else break;
+            }
+        }
+        return counter;
+    }
+}
